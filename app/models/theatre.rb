9630688870c97d3d958ab2d4bb9f6e197c3ee28d
Stillman23 <Scotttillman23@gmail.com>
2018-04-11 01:52:24 -0500

    some ticket validations added

--- app/models/theatre.rb
+++ app/models/theatre.rb
@@ -1,3 +1,5 @@
 class Theatre < ApplicationRecord
     has_many :showtimes
+
+    validates :name, :max_seating, presence: true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,37 @@ class Ticket < ApplicationRecord
     validates :show_id, :customer_name, :customer_email, presence: true
     validates :customer_email, confirmation: true
     validates_format_of :customer_email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-    validates :credit_card_number, length: { minimum: 10 }
-    validates :credit_card_number, length: { maximum: 15 }
+    validates :credit_card_number, length: { minimum: 13 }
+    validates :credit_card_number, length: { maximum: 19 }
+
+
+def create_age(ticket_params)
+    
+  end
+
+ 
+
+  def check_card_is_valid?
+    array_of_numbers = []
+    total = 0
+    credit_card_number.to_s.split(//).reverse.each_slice(2) do |x|
+      array_of_numbers << (x.last.to_i*2)
+      array_of_numbers << (x.first.to_i)
+    end
+    array_of_numbers.each do |num|
+      if num.to_i > 9
+        total += (num.to_i % 10)+1
+      else 
+        total += num.to_i
+      end
+    end
+    n = credit_card_number.to_s.split('')
+    unless total % 10 == 0 
+      errors.add(:credit_card_number, "An ERROR has occured, please try again ")
+    end
+  end
+
+  
+ 
+  
 end
