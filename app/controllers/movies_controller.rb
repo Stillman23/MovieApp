e66a45e3b84286001d213c6c33d1052639183661
Stillman23 <Scotttillman23@gmail.com>
2018-04-16 04:43:48 -0500

    fixed form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,5 @@ class MoviesController < ApplicationController
 
     def show
         @movie = Movie.find(params[:id])
-
-        
     end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,6 @@ class OrdersController < ApplicationController
     @showtimes = @movie.showtimes.collect { |time| time.showtime }
     @order.tickets.build
   end
-
   def create
     @order = Order.new(order_params)
     if @order.save
@@ -17,21 +16,8 @@ class OrdersController < ApplicationController
       render 'new'
     end
   end
-
-
   def order_params
     params.require(:order).
       permit(:customer_name, :customer_email, :credit_card_number, :credit_card_expiration_date, tickets_attributes: [:id, :price, :showtime])
   end
-    # if @tickets.save
-    #   @tickets.movie.increment!(:max_seating)
-    #   TicketMailer.ticket_receipt(@ticket).deliver_now
-    #   flash[:notice] = "Your ticket to #{@showtime.movie.title} was purchased.  Please look for an email with your receipt. "
-    #   redirect_to root_path
-    # else
-    #   @errors = @ticket.errors.full_messages
-    #   reset_cc_and_age(@ticket)
-    #   render 'tickets/new', locals: { ticket: @ticket }
-  # end
-
 end
